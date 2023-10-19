beb0eefc6aba6ffacddc8667c44dc05c44061ffc
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-10-19 12:05:49 +0530

    Add project files.

---
+++ DapperSampleWork/Models/Company.cs
@@ -0,0 +1,28 @@
+﻿namespace DapperSampleWork.Models
+{
+    public class Company
+    {
+        public int CompnyId { get; set; }
+        public string? CompanyName { get; set; }
+        public string? CompanyDescription { get; set; }
+        public string? CompanyPhone { get; set; }
+        public string? CompanyEmail { get; set; }
+        public string? CompanyAddress { get; set; }
+        public string? CompanyCity { get; set; }
+        public string? CompanyState { get; set; }
+        public string? CompanyPostalCode { get; set; }
+        public string? CompanyZip { get; set; }
+        public string? CompanyCountry { get; set; }
+        public string? CompanyWebsite { get; set; }
+        public string? CompanyLogo { get; set; }
+        public string? CompanyFacebook { get; set; }
+        public string? CompanyTwitter { get; set; }
+        public string? CompanyInstagram { get; set; }
+        public string? CompanyLinkedin { get; set;}
+        public string? CompanyYoutube { get; set;}
+        public string? CompanyGoogle { get; set;}
+        public string? CompanyPinterest { get; set;}
+        public string? CompanyVimeo { get; set;}
+    }
+    
+}
