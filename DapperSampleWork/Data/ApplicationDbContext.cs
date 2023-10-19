c9f7a3b9375dc4736d4135dd6fe8a18b286d13a5
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-10-19 12:46:00 +0530

    Db context created

---
+++ DapperSampleWork/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DapperSampleWork.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DapperSampleWork.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Company> Companies { get; set; }
+
+    }
+}
